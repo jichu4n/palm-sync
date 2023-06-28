da26570b7278a49af4640694ea909386b3c7e110
Chuan Ji <chuan@jichu4n.com>
2023-06-28 01:15:17 -0700

    Fix PADP implementation for large data chunks.
    Previously the PADP send logic was incorrect when dealing with large data chunks
    (>1k).

--- src/protocols/padp-protocol.ts
+++ src/protocols/padp-protocol.ts
@@ -38,14 +38,14 @@ export enum PadpDatagramType {
 export class PadpDatagramAttrs extends SBitmask.of(SUInt8) {
   /** Flag indicating that this is the first datagram in a PADP message. */
   @bitfield(1)
-  isFirstDatagram = false;
+  first = false;
   /** Flag indicating that this is the last datagram in a PADP message. */
   @bitfield(1)
-  isLastDatagram = false;
+  last = false;
   /** Flag denoting a memory error on the device. */
   @bitfield(1)
-  memoryError = false;
-  /** If set, the lengthOrOffset field is 4 bytes long instead of 2. */
+  errMemory = false;
+  /** If set, the sizeOrOffset field is 4 bytes long instead of 2. */
   @bitfield(1)
   isLongForm = false;
 
@@ -60,23 +60,23 @@ export class PadpDatagramHeader extends SObject {
   type = PadpDatagramType.DATA;
   /** Flags. */
   @field()
-  attrs = new PadpDatagramAttrs();
+  flags = new PadpDatagramAttrs();
   /** Size of the entire PADP message (if first datagram) or offset within the
    * PADP message (if 2nd or later datagram).
    *
    * The size of this field depends on the isLongForm flag.
    */
   @field()
-  lengthOrOffset: SUInt16BE | SUInt32BE = SUInt16BE.of(0);
+  sizeOrOffset: SUInt16BE | SUInt32BE = SUInt16BE.of(0);
 
   deserialize(buffer: Buffer, opts?: DeserializeOptions): number {
-    // First deserialize assuming lengthOrOffset is 2 bytes, which is the common case.
-    this.lengthOrOffset = SUInt16BE.of(0);
+    // First deserialize assuming sizeOrOffset is 2 bytes, which is the common case.
+    this.sizeOrOffset = SUInt16BE.of(0);
     let readOffset = super.deserialize(buffer, opts);
-    // If lengthOrOffset should have been 4 bytes, take 2 bytes from the data field.
-    if (this.attrs.isLongForm) {
-      this.lengthOrOffset = SUInt32BE.of(
-        (this.lengthOrOffset.value << 16) | buffer.readUInt16BE(readOffset)
+    // If sizeOrOffset should have been 4 bytes, take 2 bytes from the data field.
+    if (this.flags.isLongForm) {
+      this.sizeOrOffset = SUInt32BE.of(
+        (this.sizeOrOffset.value << 16) | buffer.readUInt16BE(readOffset)
       );
       readOffset += 2;
     }
@@ -86,16 +86,16 @@ export class PadpDatagramHeader extends SObject {
   serialize(opts?: SerializeOptions): Buffer {
     if (
       !(
-        (this.attrs.isLongForm &&
-          this.lengthOrOffset.getSerializedLength() === 4) ||
-        (!this.attrs.isLongForm &&
-          this.lengthOrOffset.getSerializedLength() === 2)
+        (this.flags.isLongForm &&
+          this.sizeOrOffset.getSerializedLength() === 4) ||
+        (!this.flags.isLongForm &&
+          this.sizeOrOffset.getSerializedLength() === 2)
       )
     ) {
       throw new Error(
         `Invalid PADP datagram header: ` +
-          `isLongForm = ${this.attrs.isLongForm} but ` +
-          `lengthOfOffset has length ${this.lengthOrOffset.getSerializedLength()}`
+          `isLongForm = ${this.flags.isLongForm} but ` +
+          `lengthOfOffset has length ${this.sizeOrOffset.getSerializedLength()}`
       );
     }
     return super.serialize(opts);
@@ -132,10 +132,39 @@ export class PadpDatagram extends SerializableWrapper<Buffer> {
   }
 }
 
-/** How long to wait for an ACK before resending. */
-const PADP_ACK_TIMEOUT_MS = 500;
-/** Number of times to retry sending a PADP datagram before assuming failure. */
-const PADP_SEND_MAX_RETRIES = 10;
+/** How long to wait for an ACK before resending.
+ *
+ * From Palm OS SDK.
+ */
+const PADP_ACK_WAIT_MS = 2000;
+/** Number of times to retry sending a PADP datagram before assuming failure.
+ *
+ * From Palm OS SDK.
+ */
+const PADP_MAX_RETRIES = 10;
+/** Maximum amount of data that can be sent in a single PADP packet (excluding
+ * header).
+ *
+ * From Palm OS SDK.
+ */
+const PADP_MAX_PKT_DATA_SIZE = 1024;
+/** Actual max PADP packet data size we'll try to send.
+ *
+ * While the theoretical maximum size of a PADP packet is 1024 bytes, in
+ * practice it seems like sending such large packets may cause the Palm OS to
+ * drop the connection.
+ *
+ * Possible reason:
+ *   - Just before / while we transmit a large packet, the Palm OS device may
+ *     send us additional PADP packets that it expects us to ACK within a
+ *     certain time window.
+ *   - But since we can't send back an ACK until we finish trasmitting the
+ *     current packet, the Palm OS device times out and drops the connection.
+ *
+ * So to work around this, we'll actuall use a smaller max packet size than the
+ * theoretical maximum.
+ */
+const PADP_PACKET_DATA_SIZE = PADP_MAX_PKT_DATA_SIZE / 2;
 
 /** PADP stream over a raw data stream.
  *
@@ -193,12 +222,21 @@ export class PadpStream extends Duplex {
       case PadpDatagramType.ACK:
         if (
           !this.ackListener ||
-          slpDatagram.header.xid !== this.ackListener.xid
+          slpDatagram.header.xid !== this.ackListener.xid ||
+          padpDatagram.header.sizeOrOffset.value !==
+            this.ackListener.sizeOrOffset
         ) {
-          this.log(`--- Ignoring unexpected ACK xid ${slpDatagram.header.xid}`);
+          this.log(
+            '--- Ignoring unexpected ACK xid ' +
+              `${slpDatagram.header.xid} ` +
+              `[@${padpDatagram.header.sizeOrOffset.value}]`
+          );
           return;
         }
-        this.log(`<<< ACK xid ${this.ackListener.xid}`);
+        this.log(
+          `<<< ACK xid ${this.ackListener.xid} ` +
+            `[@${this.ackListener.sizeOrOffset}]`
+        );
         this.ackListener.resolve();
         this.ackListener = null;
         return;
@@ -219,10 +257,14 @@ export class PadpStream extends Duplex {
       // If this datagram is exactly the same as the most recent PADP DATA
       // datagram we ACKed, this datagram is a duplicate as subsequent distinct
       // datagrams should either have a different transaction ID or a different
-      // lengthOrOffset. Our ACK either got lost or didn't arrive on time, so
+      // sizeOrOffset. Our ACK either got lost or didn't arrive on time, so
       // the Palm device is retrying sending the same datagram. We will ignore
       // this datagram but will send back another ACK.
-      this.log(`--- Ignoring duplicate PADP xid ${slpDatagram.header.xid}`);
+      this.log(
+        '--- Ignoring duplicate PADP xid ' +
+          `${slpDatagram.header.xid} ` +
+          `[@${padpDatagram.header.sizeOrOffset.value}]`
+      );
     } else {
       // If we just sent a message and we're still waiting for the ACK, but then
       // receive a DATA message that has the same XID, it means the current
@@ -239,7 +281,7 @@ export class PadpStream extends Duplex {
       if (this.currentMessage) {
         // If we've already received earlier datagrams for this message, ensure
         // this datagram is the next one in the sequence.
-        if (padpDatagram.header.attrs.isFirstDatagram) {
+        if (padpDatagram.header.flags.first) {
           this.emitReadError(
             'Expected PADP datagram with isFirstDatagram unset',
             {
@@ -250,7 +292,7 @@ export class PadpStream extends Duplex {
           return;
         }
         if (
-          padpDatagram.header.lengthOrOffset.value !==
+          padpDatagram.header.sizeOrOffset.value !==
           this.currentMessage.data.length
         ) {
           this.emitReadError(
@@ -261,7 +303,7 @@ export class PadpStream extends Duplex {
         }
       } else {
         // If no existing message being parsed, start parsing a new message.
-        if (!padpDatagram.header.attrs.isFirstDatagram) {
+        if (!padpDatagram.header.flags.first) {
           this.emitReadError(
             'Expected PADP datagram with isFirstDatagram set',
             {
@@ -273,7 +315,7 @@ export class PadpStream extends Duplex {
         }
         this.currentMessage = {
           data: new SmartBuffer(),
-          remainingLength: padpDatagram.header.lengthOrOffset.value,
+          remainingLength: padpDatagram.header.sizeOrOffset.value,
         };
       }
 
@@ -292,7 +334,7 @@ export class PadpStream extends Duplex {
         return;
       } else if (this.currentMessage.remainingLength === 0) {
         // If current message is complete, emit it.
-        if (!padpDatagram.header.attrs.isLastDatagram) {
+        if (!padpDatagram.header.flags.last) {
           this.emitReadError(
             'Expected final PADP datagram to have isLastDatagram set',
             {slpDatagram, padpDatagram}
@@ -306,7 +348,7 @@ export class PadpStream extends Duplex {
         this.currentMessage = null;
       } else {
         // Current message is not yet complete.
-        if (padpDatagram.header.attrs.isLastDatagram) {
+        if (padpDatagram.header.flags.last) {
           this.emitReadError(
             'Expected non-final PADP datagram to have isLastDatagram unset',
             {slpDatagram, padpDatagram}
@@ -320,7 +362,8 @@ export class PadpStream extends Duplex {
     const ackSlpDatagram = this.createAckSlpDatagram(slpDatagram, padpDatagram);
     this.lastProcessedPadpDataChunk = chunk;
     this.log(
-      `>>> ACK xid ${ackSlpDatagram.header.xid}: ` +
+      `>>> ACK xid ${ackSlpDatagram.header.xid} ` +
+        `[@${padpDatagram.header.sizeOrOffset.value}]: ` +
         ackSlpDatagram.serialize().toString('hex')
     );
     this.slpDatagramStream.write(
@@ -354,26 +397,26 @@ export class PadpStream extends Duplex {
     // datagram.
     const pieces: Array<Buffer> = [];
     for (let startOffset = 0; startOffset < chunk.length; ) {
-      const endOffset = startOffset + 1024;
+      const endOffset = startOffset + PADP_PACKET_DATA_SIZE;
       pieces.push(chunk.slice(startOffset, endOffset));
       startOffset = endOffset;
     }
 
+    const xid = this.getNextXid();
     let bytesWritten = 0;
     for (const [i, piece] of pieces.entries()) {
       const padpDatagram = new PadpDatagram();
       padpDatagram.header = PadpDatagramHeader.with({
         type: PadpDatagramType.DATA,
-        attrs: PadpDatagramAttrs.with({
-          isFirstDatagram: i === 0,
-          isLastDatagram: i === pieces.length - 1,
+        flags: PadpDatagramAttrs.with({
+          first: i === 0,
+          last: i === pieces.length - 1,
         }),
-        lengthOrOffset: SUInt16BE.of(i === 0 ? chunk.length : bytesWritten),
+        sizeOrOffset: SUInt16BE.of(i === 0 ? chunk.length : bytesWritten),
       });
       padpDatagram.value = piece;
       bytesWritten += piece.length;
 
-      const xid = this.getNextXid();
       const slpDatagram = new SlpDatagram();
       slpDatagram.header = SlpDatagramHeader.with({
         destSocketId: SlpSocketId.DLP,
@@ -385,19 +428,22 @@ export class PadpStream extends Duplex {
       slpDatagram.value = padpDatagram.serialize();
 
       let error: Error | null = null;
-      for (let retryId = 0; retryId < PADP_SEND_MAX_RETRIES; ++retryId) {
+      for (let retryId = 0; retryId < PADP_MAX_RETRIES; ++retryId) {
         error = null;
 
         // Write SLP and wait for confirmation.
+        const slpDatagramBuffer = slpDatagram.serialize();
         this.log(
-          `>>> PADP xid ${xid}: ` +
-            slpDatagram.serialize().toString('hex') +
+          `>>> PADP xid ${xid} ` +
+            `${i + 1}/${pieces.length} ` +
+            `[@${padpDatagram.header.sizeOrOffset.value}]: ` +
+            slpDatagramBuffer.toString('hex') +
             (retryId > 0 ? ` (try #${retryId + 1})` : '')
         );
         try {
           await new Promise<void>((resolve, reject) =>
             this.slpDatagramStream.write(
-              slpDatagram.serialize(),
+              slpDatagramBuffer,
               'buffer' as BufferEncoding,
               (error) => (error ? reject(error) : resolve())
             )
@@ -415,11 +461,25 @@ export class PadpStream extends Duplex {
           callback(new Error('Internal error: multiple concurrent writes'));
           return;
         }
-        this.log(`--- Waiting for ACK on xid ${xid}`);
+        this.log(
+          `--- Waiting for ACK on xid ${xid} ` +
+            `${i + 1}/${pieces.length} ` +
+            `[@${padpDatagram.header.sizeOrOffset.value}]`
+        );
         try {
           await new Promise<void>((resolve, reject) => {
-            this.ackListener = {resolve, reject, xid};
-            setTimeout(() => reject(new Error('Timeout')), PADP_ACK_TIMEOUT_MS);
+            this.ackListener = {
+              resolve,
+              reject,
+              xid,
+              sizeOrOffset: padpDatagram.header.sizeOrOffset.value,
+            };
+            setTimeout(
+              () => reject(new Error('Timeout')),
+              // Use a smaller timeout when running inside tests to avoid
+              // hanging test workers.
+              process.env.JEST_WORKER_ID ? 100 : PADP_ACK_WAIT_MS
+            );
           });
         } catch (e: any) {
           error = new Error(
@@ -438,7 +498,7 @@ export class PadpStream extends Duplex {
       // return that to the caller.
       if (error) {
         this.log(
-          `--- PADP xid ${xid} failed after ${PADP_SEND_MAX_RETRIES} retries`
+          `--- PADP xid ${xid} failed after ${PADP_MAX_RETRIES} retries`
         );
         callback(error);
         return;
@@ -455,12 +515,12 @@ export class PadpStream extends Duplex {
     const padpDatagram = new PadpDatagram();
     padpDatagram.header = PadpDatagramHeader.with({
       type: PadpDatagramType.ACK,
-      attrs: PadpDatagramAttrs.with({
-        isFirstDatagram: true,
-        isLastDatagram: true,
+      flags: PadpDatagramAttrs.with({
+        first: true,
+        last: true,
       }),
-      lengthOrOffset: SUInt16BE.of(
-        receivedPadpDatagram.header.lengthOrOffset.value
+      sizeOrOffset: SUInt16BE.of(
+        receivedPadpDatagram.header.sizeOrOffset.value
       ),
     });
     const slpDatagram = new SlpDatagram();
@@ -517,6 +577,7 @@ export class PadpStream extends Duplex {
     resolve: () => void;
     reject: (error: Error) => void;
     xid: number;
+    sizeOrOffset: number;
   } | null = null;
 
   /** Next transaction ID to use for sending, incremented with every SLP
