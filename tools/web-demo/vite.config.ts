a207526ec703edb1794fdc91ef38167e39688f37
Chuan Ji <chuan@jichu4n.com>
2024-04-08 23:31:46 -0700

    Fix web demo deployment on GitHub Pages.

--- tools/web-demo/vite.config.ts
+++ tools/web-demo/vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './',
   // We need to explicitly configure Vite to treat palm-sync as CommonJS module
   // as it is in the same monorepo. See documentation:
   // https://vitejs.dev/guide/dep-pre-bundling#monorepos-and-linked-dependencies
