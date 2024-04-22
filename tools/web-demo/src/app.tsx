544af6caa7fb6e948eec4fbc28e14594e88e4778
Chuan Ji <chuan@jichu4n.com>
2024-04-22 08:56:37 -0700

    [Web demo] Add banner when WebUSB and/or Web Serial isn't supported.

--- tools/web-demo/src/app.tsx
+++ tools/web-demo/src/app.tsx
@@ -2,8 +2,35 @@ import Box from '@mui/material/Box';
 import {ActionPanel} from './action-panel';
 import {DeviceInfoPanel} from './device-info-panel';
 import {LogViewer} from './log-viewer';
+import Typography from '@mui/material/Typography';
 
 export function App() {
+  if (!navigator.serial || !navigator.usb) {
+    const unsupportedApis = [
+      ...(navigator.serial ? [] : ['Web Serial']),
+      ...(navigator.usb ? [] : ['WebUSB']),
+    ].join(' and ');
+    return (
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+          height: '100vh',
+        }}
+      >
+        <Typography variant="h4" gutterBottom>
+          This browser does not support {unsupportedApis} APIs.
+        </Typography>
+        <Typography variant="body1">
+          Please use a Chromium-based browser and ensure USB and serial support
+          are enabled.
+        </Typography>
+      </div>
+    );
+  }
+
   return (
     <Box sx={{position: 'fixed', height: 1, width: 1, display: 'flex'}}>
       <Box
